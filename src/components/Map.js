a1b5848fb7bf7cf96dd35c9bc9873eac0dfc6d9a
ernven <ernesto.venzano.rodriguez@myy.haaga-helia.fi>
2021-09-30 12:54:50 +0300

    Fixed map display, extended it to show any number of floors.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,100 +1,52 @@
-import { useEffect, useState } from 'react';
-
-
+import { useEffect, useState } from 'react'
 
 export default function Map({Data}) {
 
-    const [floor1amount, setFloor1amount] = useState(0);
-    const [floor2amount, setFloor2amount] = useState(0);
-    const [floor3amount, setFloor3amount] = useState(0);
-    const [floor4amount, setFloor4amount] = useState(0);
+    const [mapValues, setMapValues] = useState([])
+
 
     useEffect(() => {
-        
-        Data.forEach(post =>
-            updateRooms(post.floor, post.status)
-        )
+        let toSet = []
+
+        // Since we don't know which floors have rooms in each building, we have to iterate through the array.
+        Data.forEach(entry => {
+
+            // If the floor isn't in the array yet, add it.
+            if (!toSet.some(o => o.floor === entry.floor)) { toSet.push({floor: entry.floor, freeRooms: 0}) }
+
+            // If the room is available, add it to the count.
+            if (entry.status === true) {
+                toSet[entry.floor - 1].freeRooms++
+            }
+
+        })
+
+        setMapValues(toSet)
         
     }, [Data])
-    var i = 0;
-    const updateRooms = (floor, status) => {
-        i++
-        console.log("Times run: " + i)
-        
-        console.log("floor: " + floor + " status: " + status)
-        
-        if (floor === 1) {
-            
-            console.log("floor amount 1: " + floor)
-            if (status === true) {
-                setFloor1amount(floor1amount + 1);
-                
-        
-            }
-        }
-        if (floor === 2) {
-            
-            if (status === true) {
-                setFloor2amount(floor2amount + 1);
-                
-             }
-        }
-        if (floor === 3) {
-            if (status === true) {
-                setFloor3amount(floor3amount + 1);
-                
-             }
-        }
-        if (floor === 4) {
-            if (status === true) {
-                setFloor4amount(floor4amount + 1);
-            
-             }
-        }
-    }
-
-    
 
 
-    const changeColor1 = (amount) => {
-        console.log("testdata 1 :" + amount)
+    const changeColor = (amount) => {
         if (amount === 0) {
-           return <p id="floor1boxRed">{amount} available rooms</p>
+           return <p id="floorboxRed">{amount} available rooms</p>
         } else {
-            return <p id="floor1boxGreen">{amount} available rooms</p>
+            return <p id="floorboxGreen">{amount} available rooms</p>
         }
     }
 
-    const changeColor2 = (amount) => {
-        if (amount === 0) {
-            return <p id="floor2boxRed">{amount} available rooms</p>
-         } else {
-             return <p id="floor2boxGreen">{amount} available rooms</p>
-         }
-    }
+    const mapBuilder = () => {
+        let boxes = []
 
-    const changeColor3 = (amount) => {
-        if (amount === 0) {
-            return <p id="floor3boxRed">{amount} available rooms</p>
-         } else {
-             return <p id="floor3boxGreen">{amount} available rooms</p>
-         }
-    }
+        for (let i = mapValues.length; i > 0; i--) {
+            boxes.push(<div className="floorLabel" key={i}>{changeColor(mapValues[i-1].freeRooms)}{i}th floor</div>)
+        }
 
-    const changeColor4 = (amount) => {
-        if (amount === 0) {
-            return <p id="floor4boxRed">{amount} available rooms</p>
-         } else {
-             return <p id="floor4boxGreen">{amount} available rooms</p>
-         }
+        return boxes
     }
 
     return (
         <div id="map">
-            <div className="floorLabel">{changeColor4(floor4amount)}4th floor</div>
-            <div className="floorLabel">{changeColor3(floor3amount)}3rd floor</div>
-            <div className="floorLabel">{changeColor2(floor2amount)}2nd floor</div>
-            <div className="floorLabel">{changeColor1(floor1amount)}1st floor</div>
+            {mapBuilder()}
         </div>
     )
 
